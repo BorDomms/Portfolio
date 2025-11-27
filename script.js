2e3a16b4fc591e304e2b5541d165833e8482c505
BorDomms <drLimjap@mcm.edu.ph>
2025-11-27 15:25:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
+
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark');
+}
+
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark');
+
+    if(body.clasList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
 });
